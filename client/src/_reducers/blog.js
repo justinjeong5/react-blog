0945716780adc6ba53fe65215dcd432ab31c5623
justinjeong <justin.jeong5@gmail.com>
2020-12-14 13:08:03 +0900

    initial commit for redux-sage for blogPage

---
+++ client/src/_reducers/blog.js
@@ -0,0 +1,43 @@
+import {
+  CREATE_BLOG_POST_REQUEST, CREATE_BLOG_POST_SUCCESS, CREATE_BLOG_POST_FAILURE,
+} from '../_sagas/types'
+
+const initialState = {
+  createBlogPostLoading: false,
+  createBlogPostDone: false,
+  createBlogPostError: null,
+
+  blogPostData: null,
+}
+
+const blog = (state = initialState, action) => {
+  switch (action.type) {
+    case CREATE_BLOG_POST_REQUEST:
+      return {
+        ...state,
+        createBlogPostLoading: true,
+        createBlogPostDone: false,
+        createBlogPostError: null,
+      }
+    case CREATE_BLOG_POST_SUCCESS:
+      return {
+        ...state,
+        createBlogPostLoading: false,
+        createBlogPostDone: true,
+        blogPostData: action.payload,
+      }
+    case CREATE_BLOG_POST_FAILURE:
+      return {
+        ...state,
+        createBlogPostLoading: false,
+        createBlogPostError: action.error
+      }
+
+    default:
+      return {
+        ...state
+      }
+  }
+}
+
+export default blog;
---
+++ client/src/_sagas/blog.js
@@ -0,0 +1,37 @@
+import { all, fork, put, call, takeLatest } from 'redux-saga/effects'
+import axios from 'axios';
+
+import {
+  CREATE_BLOG_POST_REQUEST, CREATE_BLOG_POST_SUCCESS, CREATE_BLOG_POST_FAILURE,
+} from './types'
+
+function createPostAPI(data) {
+  return axios.post('/api/blog/createPost', data)
+}
+
+function* createPost(action) {
+  try {
+    const result = yield call(createPostAPI, action.payload);
+    yield put({
+      type: CREATE_BLOG_POST_SUCCESS,
+      payload: result.data,
+    })
+  } catch (error) {
+    console.error(error)
+    yield put({
+      type: CREATE_BLOG_POST_FAILURE,
+      error: error.response.data,
+    })
+  }
+}
+
+function* watchCreatePost() {
+  yield takeLatest(CREATE_BLOG_POST_REQUEST, createPost)
+}
+
+
+export default function* userSaga() {
+  yield all([
+    fork(watchCreatePost),
+  ])
+}
