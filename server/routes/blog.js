f1619227b238443c83d76701a04887aed4942e2e
justinjeong <justin.jeong5@gmail.com>
2020-12-14 14:34:04 +0900

    feat(server): save blog post for mongodb ->  "/api/blog/createPost"

--- server/routes/blog.js
+++ server/routes/blog.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const { Blog } = require('../models/Blog')
 
 //=====================================
 //                Blog
 //=====================================
 
 
-router.post('/post', (req, res) => {
+router.post('/createPost', (req, res) => {
+  const blog = new Blog(req.body);
+
+  blog.save((error, postInfo) => {
+    if (error) {
+      return res.status(401).json({ code: 'DatabaseError', message: '블로그 글을 저장하는 과정에서 문제가 발생했습니다.', error });
+    }
+    return res.status(200).json({ success: true, postInfo })
+  })
 
 })
 
